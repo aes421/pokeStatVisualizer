a6853987205197686af7578d3f8b958f3d81c9bc
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-09-30 21:53:26 -0500

    very rough draft of visualizing data returned with d3

--- serverSide.php
+++ serverSide.php
@@ -1,14 +1,12 @@
 <?php
 	//connect to database
 	$db_params = parse_ini_file('/home/pi/Desktop/db_params.ini');
-	echo "Hello, world";
 	
 	try{
 		$dsn = "mysql:host=".$db_params['host'].";dbname=PokeStats";
 		$conn = new PDO($dsn, $db_params['user'], $db_params['password']);
 		//set the PDO error mode to exception
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	echo "Connected successfully"; 
 
     	$query = $conn->prepare("SELECT pokemon.identifier, pokemon_stats.base_stat FROM pokemon_stats INNER JOIN pokemon ON pokemon_stats.pokemon_id = pokemon.id WHERE pokemon.id = 1 AND pokemon_stats.stat_id = :selected_stat");
     	$query->execute(array(':selected_stat' => $_GET['stat']));
--- visualizer.js
+++ visualizer.js
@@ -1,5 +1,18 @@
-function visualize(){
+function visualize(json){
 		var svgContainer = d3.select("body").append("svg").attr("width", 400).attr("height", 100);
+
+		var data = JSON.parse(json);
+		var circles = svgContainer
+ 		.selectAll("circle")
+ 		.data(data)
+ 		.enter()
+ 		.append("circle");
+
+ 		var circleAttributes = circles
+                        .attr("cx", function (d) { return 40; })
+                        .attr("cy", function (d) { return 40; })
+                        .attr("r", function (d) { return 40; })
+                        .style("fill", function (d) { return "red"; });
 	}
 
 
@@ -14,6 +27,7 @@ function lookupStats(stat){
     }
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+        	visualize(xmlhttp.responseText);
         	document.getElementById("connectionInfo").innerHTML = xmlhttp.responseText;
         }
     };
