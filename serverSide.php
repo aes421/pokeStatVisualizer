b7be0b50d9e8913e909704a844a9971a1d3eba58
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-09-30 19:38:42 -0500

    finally got the server setup

--- serverSide.php
+++ serverSide.php
@@ -1,3 +1,16 @@
 <?php
-	echo phpinfo();
+	//connect to database
+	$db_params = parse_ini_file('/home/pi/Desktop/db_params.ini');
+	echo "Hello, world";
+	
+	try{
+		$dsn = "mysql:host=".$db_params['host'].";dbname=PokeStats";
+		$conn = new PDO($dsn, $db_params['user'], $db_params['password']);
+		//set the PDO error mode to exception
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	echo "Connected successfully"; 
+	}
+	catch(PDOException $e){
+		echo "Connection failed: " . $e->getMessage();
+	}
 ?>
