549df4b19b5fbdc1982c5532be7cc0c918cb2430
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-10-02 21:49:04 -0500

    switched to a force layout and have bubbles visualizing now

--- visualizer.js
+++ visualizer.js
@@ -4,40 +4,63 @@ function visualize(json){
 	var width = svgContainer.attr("width");
 	var height = svgContainer.attr("height");
 
-	var pack = d3.pack()
-    .size([width, height])
-    .padding(1.5);
-	
 	var data = JSON.parse(json);
 
-	//var root = d3.hierarchy(data);
-	var root = d3.hierarchy(({children: data}));
+    var forceSimulation = d3.forceSimulation(data)
+		.force('charge', d3.forceManyBody().strength(-20)) 
+  		.force('center', d3.forceCenter(width / 2, height / 2)); 
 
-	var node = svgContainer.selectAll(".node")
-    .data(pack(root).leaves())
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  	// var node = svgContainer.selectAll(".node")
+   //  .data(data)
+   //  .enter().append("g")
+   //    .attr("class", "node");
 
-    var circles = node
-		.selectAll("circle")
-		.data(data)
-		.enter()
-		.append("circle")
-	        .attr("r", function (d) { return d.base_stat; })
-	        .style("fill", function (d) { return "red"; });
+  //   var circles = node
+		// .selectAll("circle")
+		// .data(data)
+		// .enter()
+		// .append("circle")
+	 //        .attr("r", function (d) { return d.base_stat; })
+	 //        .style("fill", function (d) { return "red"; });
 		
-     node.selectAll("text")
- 		.data(data)
- 		.enter()
- 		.append("text")
- 			.text( function(d) { return d.identifier; });
+  //    node.selectAll("text")
+ 	// 	.data(data)
+ 	// 	.enter()
+ 	// 	.append("text")
+ 	// 		.text( function(d) { return d.identifier; });
+
+ 	var node = svgContainer.append('g')
+ 		 .selectAll('circle')
+ 		 .data(data)
+ 		 .enter()
+ 		 .append('circle')
+ 		 	 .attr("r", function (d) { return d.base_stat; })
+	          .style("fill", function (d) { return "red"; });
+
+	var text = svgContainer
+	.append('g')
+	.selectAll("text")
+ 	.data(data)
+ 	.enter()
+ 	.append("text")
+ 		.text( function(d) { return d.identifier; });
+
+
+ 	forceSimulation.nodes(data).on("tick", ticked);
+
+ 	function ticked() {
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+    text
+    	.attr("x", function(d) { return d.x; })
+    	.attr("y", function(d) { return d.y; });
+  }
 }
 
 function reset(){
 	var svgContainer = d3.select("svg");
 	svgContainer.selectAll("*").remove();
-	//d3.select("svg").remove();
 }
 
 
