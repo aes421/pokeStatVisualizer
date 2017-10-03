10f42e55d144d71fb13fc8ddb7a78de75089a761
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-10-03 18:01:57 -0500

    a whole lot done... applied a linear scale to the stats, changed the forces on the simulation, and now gets all pokemon instead of a few

--- visualizer.js
+++ visualizer.js
@@ -10,7 +10,7 @@ function visualize(json){
 		['8', ''],  //ghost
 		['9', ''],  //steel
 		['10', '#FF0000'], //fire
-		['11', ''], //water
+		['11', '#0000FF'], //water
 		['12', '#008000'], //grass
 		['13', ''], //electric
 		['14', ''], //psychic
@@ -27,13 +27,22 @@ function visualize(json){
 	var height = svgContainer.attr("height");
 
 	var data = JSON.parse(json);
+	//if we keep the data sorted we could changed this to just take the first and last
+	var max = Math.max.apply(Math, data.map(function(o){return o.base_stat;}));
+	var min = Math.min.apply(Math, data.map(function(o){return o.base_stat;}));;
+	//Blissey has max hp at 255
+	var linearScale = d3.scaleLinear().domain([min, max]).range([0,100]);
+
+	for (var i = 0; i < data.length; i++){
+			data[i].base_stat = linearScale(data[i].base_stat);
+	}
 
     var forceSimulation = d3.forceSimulation(data)
-		.force('charge', d3.forceManyBody().strength(-20)) 
+		.force('charge', d3.forceManyBody().strength(0)) 
   		.force('center', d3.forceCenter(width / 2, height / 2))
   		.force('collision', d3.forceCollide().radius(function(d) {
     		return d.base_stat;
-  		})); 
+		})); 
 
  	var node = svgContainer.append('g')
  		 .selectAll('circle')
@@ -83,7 +92,7 @@ function lookupStats(stat){
         if (this.readyState == 4 && this.status == 200) {
         	reset();
         	visualize(xmlhttp.responseText);
-        	document.getElementById("connectionInfo").innerHTML = xmlhttp.responseText;
+        	//document.getElementById("connectionInfo").innerHTML = xmlhttp.responseText;
         }
     };
     xmlhttp.open("GET","serverSide.php?stat="+stat,true);
